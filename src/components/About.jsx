4c43e759253c88627fd2d7463927021b9cdf2a2c
Navneet Kishan Srinivasan <jzd5gy@inf.elte.hu>
2025-07-14 21:29:46 +0200

    minor fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,16 +28,31 @@ export default function About() {
               I love building performant web applications and exploring new technologies.
               <br />
               Always curious and eager to learn.
+                            Hello! I'm a passionate developer and researcher.
+              <br />
+              I love building performant web applications and exploring new technologies.
+              <br />
+              Always curious and eager to learn.
+                            Hello! I'm a passionate developer and researcher.
+              <br />
+              I love building performant web applications and exploring new technologies.
+              <br />
+              Always curious and eager to learn.
             </p>
           </div>
         </div>
         <div className="image-area">
-          <img
-            src={`/images/about/${aboutImages[current].filename}`}
-            alt={aboutImages[current].caption}
-          />
+          {aboutImages.map((img, index) => (
+            <img
+              key={img.filename}
+              src={`/images/about/${img.filename}`}
+              alt={img.caption}
+              className={index === current ? "active" : ""}
+            />
+          ))}
           <div className="caption">{aboutImages[current].caption}</div>
         </div>
+
       </div>
     </section>
   );
